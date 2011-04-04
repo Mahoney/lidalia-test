c892da0e8c28b5268165f6d3b6533f013b2eea35
Robert Elliot <rob@lidalia.org.uk>
2011-04-04 08:25:25 +0100

    Added shouldThrow methods that take a Runnable - will improve Groovy integration and make for a lighter usage if exception thrown is unchecked

--- src/main/java/uk/org/lidalia/test/Assert.java
+++ src/main/java/uk/org/lidalia/test/Assert.java
@@ -9,12 +9,12 @@ import java.util.concurrent.Callable;
 
 public class Assert {
 
-	public static <ThrowableType extends Throwable> ThrowableType shouldThrow(Class<ThrowableType> expectedThrowableType, Callable<Void> workThatShouldThrowThrowable) throws Throwable {
+	public static <ThrowableType extends Throwable> ThrowableType shouldThrow(final Class<ThrowableType> expectedThrowableType, final Callable<Void> workThatShouldThrowThrowable) throws Throwable {
 		return shouldThrow(expectedThrowableType, null, workThatShouldThrowThrowable);
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <ThrowableType extends Throwable> ThrowableType shouldThrow(Class<ThrowableType> expectedThrowableType, String message, Callable<Void> workThatShouldThrowThrowable) throws Throwable {
+	public static <ThrowableType extends Throwable> ThrowableType shouldThrow(final Class<ThrowableType> expectedThrowableType, final String message, final Callable<Void> workThatShouldThrowThrowable) throws Throwable {
 		try {
 			workThatShouldThrowThrowable.call();
 		} catch (Throwable actualThrowableThrown) {
@@ -24,16 +24,40 @@ public class Assert {
 				throw actualThrowableThrown;
 			}
 		}
-		String realMessage = message == null ? "No exception thrown" : message;
+		final String realMessage = message == null ? "No exception thrown" : message;
 		throw new AssertionError(realMessage);
 	}
 
-	public static <ThrowableType extends Throwable> void shouldThrow(final ThrowableType expectedThrowable, Callable<Void> workThatShouldThrowThrowable) throws Throwable {
+	public static <ThrowableType extends Throwable> void shouldThrow(final ThrowableType expectedThrowable, final Callable<Void> workThatShouldThrowThrowable) throws Throwable {
 		shouldThrow(expectedThrowable, null, workThatShouldThrowThrowable);
 	}
 
-	public static <ThrowableType extends Throwable> void shouldThrow(final ThrowableType expectedThrowable, String message, Callable<Void> workThatShouldThrowThrowable) throws Throwable {
-		ThrowableType actualThrowable = shouldThrow(getClass(expectedThrowable), message, workThatShouldThrowThrowable);
+	public static <ThrowableType extends Throwable> void shouldThrow(final ThrowableType expectedThrowable, final String message, final Callable<Void> workThatShouldThrowThrowable) throws Throwable {
+		final ThrowableType actualThrowable = shouldThrow(getClass(expectedThrowable), message, workThatShouldThrowThrowable);
+		assertSame(message, expectedThrowable, actualThrowable);
+	}
+
+	public static <ThrowableType extends Throwable> ThrowableType shouldThrow(final Class<ThrowableType> expectedThrowableType, final Runnable workThatShouldThrowThrowable) throws Throwable {
+		return shouldThrow(expectedThrowableType, null, workThatShouldThrowThrowable);
+	}
+
+	public static <ThrowableType extends Throwable> ThrowableType shouldThrow(final Class<ThrowableType> expectedThrowableType, final String message, final Runnable workThatShouldThrowThrowable) throws Throwable {
+		final Callable<Void> workAsCallable = new Callable<Void>() {
+			@Override
+			public Void call() throws Exception {
+				workThatShouldThrowThrowable.run();
+				return null;
+			}
+		};
+		return shouldThrow(expectedThrowableType, message, workAsCallable);
+	}
+
+	public static <ThrowableType extends Throwable> void shouldThrow(final ThrowableType expectedThrowable, final Runnable workThatShouldThrowThrowable) throws Throwable {
+		shouldThrow(expectedThrowable, null, workThatShouldThrowThrowable);
+	}
+
+	public static <ThrowableType extends Throwable> void shouldThrow(final ThrowableType expectedThrowable, final String message, final Runnable workThatShouldThrowThrowable) throws Throwable {
+		final ThrowableType actualThrowable = shouldThrow(getClass(expectedThrowable), message, workThatShouldThrowThrowable);
 		assertSame(message, expectedThrowable, actualThrowable);
 	}
 
@@ -41,15 +65,15 @@ public class Assert {
 	public static void assertNotInstantiable(final Class<?> classThatShouldNotBeInstantiable) throws Throwable {
 		assertOnlyHasNoArgsConstructor(classThatShouldNotBeInstantiable);
 
-		InvocationTargetException invocationTargetException = shouldThrow(InvocationTargetException.class, new Callable<Void>() {
+		final InvocationTargetException invocationTargetException = shouldThrow(InvocationTargetException.class, new Callable<Void>() {
 			public Void call() throws Exception {
-				Constructor<?> constructor = classThatShouldNotBeInstantiable.getDeclaredConstructor();
+				final Constructor<?> constructor = classThatShouldNotBeInstantiable.getDeclaredConstructor();
 				constructor.setAccessible(true);
 				constructor.newInstance();
 				return null;
 			}
 		});
-		UnsupportedOperationException cause = (UnsupportedOperationException) invocationTargetException.getCause();
+		final UnsupportedOperationException cause = (UnsupportedOperationException) invocationTargetException.getCause();
 		assertEquals("Not instantiable", cause.getMessage());
 	}
 
@@ -60,7 +84,7 @@ public class Assert {
 		assertEquals(0, constructor.getParameterTypes().length);
 	}
 	
-	private static boolean instanceOf(Object o, Class<?> c) {
+	private static boolean instanceOf(final Object o, final Class<?> c) {
 		return c.isAssignableFrom(o.getClass());
 	}
 	
